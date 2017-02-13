b83b0c292924087902f8e9b8354f83145ca1355f
sunayanavrao <suvr16cs@cmrit.ac.in>
2017-02-13 15:45:35 +0530

    Update main.c

---
+++ week1/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+  int a=3,b=2;
+    printf("The sum of a and b is= %d \n",add(a,b));
+  return 0;
+}
+           int add(int a,int b)
+           {
+               return (a+b);
+           }
+                   
