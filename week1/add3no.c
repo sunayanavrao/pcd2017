152e1bcb7e16c918e1b9d8bed1f1f9e429f38434
Sunayana <suvr16cs@cmrit.ac.in>
2017-03-01 15:59:49 +0530

    new

---
+++ week1/add3no.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() 
+{
+int a=3,b=2,c=4;
+printf("The sum of a,b and c is= %d \n",add(a,b,c));
+return 0;
+}
+int add(int a,int b,int c)
+{
+return (a+b+c);
+}
+
---
+++ week1/addncomplex.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int getcomplex(float a[][],int n)
+{
+for(int i=0;i<n;i++) 
+{
+for(int j=0;j<=1;j++)
+{
+scanf("%f",(&(a[i][j]))); }
+}                                                
+return 5;
+}                                                
+int showcomplex(float c,float d,int n) {
+printf("Result:Sum of %d complex no.s is",n);    
+if(d<0){ printf("%f%fi",c,d);}
+else{ printf("%f + %fi",c,d);}
+}
+int  main()
+{
+int n;
+float a[15][2];
+printf("Enter the no. Of complex no.s to be adde$
+scanf("%d",&n);
+getcomplex(a[n][2],n);
+float c,d=0;
+for(int k=0;k<n;k++) { c=c+a[k][0];
+}
+for(int l=0;l<n;l++) { d=d+a[l][1];
+}
+showcomplex(c,d,n); return 2;
+}
+
