6d815de6c175556aca2b3e1542937830db482046
sunayanavrao <suvr16cs@cmrit.ac.in>
2017-04-05 04:57:12 +0530

    Update palindrome.c

--- week2/palindrome.c
+++ week2/palindrome.c
@@ -1,30 +1,25 @@
+GNU nano 2.7.4         File: paln.c
+
 #include<stdio.h>
 int palindrome(int n)
 {
-int sum=0;
-int temp;
+ int sum=0;
 while(n!=0)
 {
-temp=n%10;
-sum=(sum*10)+temp;
+sum=(sum*10)+(n%10);
 n=n/10;
 }
 return sum;
-}
-int main()
-{
-int n,sum;
+}                                                       int main()
+{                                                       int n,sum;
 printf("enter a no. \n");
 scanf("%d",&n);
 sum=palindrome(n);
 if(sum==n)
-{ printf("%d is a palindrome",n);
-}
-else 
 {
-printf("%d is not a palindrome",n);
+printf("%d is a palindrome",n);
+}
+else { printf("%d is not a palindrome",n);
 }
 return 0;
 }
-
-
